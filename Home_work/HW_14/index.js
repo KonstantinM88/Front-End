3d4999ecbdd6c7c168eec6ec3b0e839da3c34fb8
KonsnantinM <mihajlov8@gmail.com>
2025-05-11 20:59:32 +0200

    hw_14

---
+++ Home_work/HW_14/index.js
@@ -0,0 +1,25 @@
+// найдем первую кнопку по id
+const btn = document.getElementById("magic-btn");
+// скопируем ее - получилась вторая кнопка. Ее можно использовать.
+const clonedBtn = btn.cloneNode(true);
+// задали id и текст
+clonedBtn.id = "magic-btn-2";
+clonedBtn.textContent = "Я изменю тебя";
+
+// применим стили
+clonedBtn.style.backgroundColor = "#a78b71";
+clonedBtn.style.color = "white";
+
+// обработчик клика по первой кнопке
+btn.addEventListener('click', () => {
+  // добавим вторую кнопку в документ, только если её ещё нет
+  if (!document.getElementById("magic-btn-2")) {
+    document.body.appendChild(clonedBtn);
+  }
+});
+
+// обработчик клика по второй кнопке
+clonedBtn.addEventListener('click', () => {
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";
+});
